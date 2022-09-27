18903ccd7508bee2947504d94e18c2ef9f2f9a38
Vipin Kumar <58135705+devVipin01@users.noreply.github.com>
2022-09-27 08:38:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,112 +58,112 @@ def show_images(batch: th.Tensor):
     print(os.getcwd())
     display(Image.fromarray(reshaped.numpy()))
 
-# Sampling parameters
-prompt = "elephant eating sugarcane and dog"
-batch_size = 1
-guidance_scale = 3.0
+# # Sampling parameters
+# prompt = "elephant eating sugarcane and dog"
+# batch_size = 1
+# guidance_scale = 3.0
 
-# Tune this parameter to control the sharpness of 256x256 images.
-# A value of 1.0 is sharper, but sometimes results in grainy artifacts.
-upsample_temp = 0.997
+# # Tune this parameter to control the sharpness of 256x256 images.
+# # A value of 1.0 is sharper, but sometimes results in grainy artifacts.
+# upsample_temp = 0.997
 
-##############################
-# Sample from the base model #
-##############################
+# ##############################
+# # Sample from the base model #
+# ##############################
 
-# Create the text tokens to feed to the model.
-tokens = model.tokenizer.encode(prompt)
-tokens, mask = model.tokenizer.padded_tokens_and_mask(
-    tokens, options['text_ctx']
-)
+# # Create the text tokens to feed to the model.
+# tokens = model.tokenizer.encode(prompt)
+# tokens, mask = model.tokenizer.padded_tokens_and_mask(
+#     tokens, options['text_ctx']
+# )
 
-# Create the classifier-free guidance tokens (empty)
-full_batch_size = batch_size * 2
-uncond_tokens, uncond_mask = model.tokenizer.padded_tokens_and_mask(
-    [], options['text_ctx']
-)
+# # Create the classifier-free guidance tokens (empty)
+# full_batch_size = batch_size * 2
+# uncond_tokens, uncond_mask = model.tokenizer.padded_tokens_and_mask(
+#     [], options['text_ctx']
+# )
 
-# Pack the tokens together into model kwargs.
-model_kwargs = dict(
-    tokens=th.tensor(
-        [tokens] * batch_size + [uncond_tokens] * batch_size, device=device
-    ),
-    mask=th.tensor(
-        [mask] * batch_size + [uncond_mask] * batch_size,
-        dtype=th.bool,
-        device=device,
-    ),
-)
+# # Pack the tokens together into model kwargs.
+# model_kwargs = dict(
+#     tokens=th.tensor(
+#         [tokens] * batch_size + [uncond_tokens] * batch_size, device=device
+#     ),
+#     mask=th.tensor(
+#         [mask] * batch_size + [uncond_mask] * batch_size,
+#         dtype=th.bool,
+#         device=device,
+#     ),
+# )
 
-# Create a classifier-free guidance sampling function
-def model_fn(x_t, ts, **kwargs):
-    half = x_t[: len(x_t) // 2]
-    combined = th.cat([half, half], dim=0)
-    model_out = model(combined, ts, **kwargs)
-    eps, rest = model_out[:, :3], model_out[:, 3:]
-    cond_eps, uncond_eps = th.split(eps, len(eps) // 2, dim=0)
-    half_eps = uncond_eps + guidance_scale * (cond_eps - uncond_eps)
-    eps = th.cat([half_eps, half_eps], dim=0)
-    return th.cat([eps, rest], dim=1)
+# # Create a classifier-free guidance sampling function
+# def model_fn(x_t, ts, **kwargs):
+#     half = x_t[: len(x_t) // 2]
+#     combined = th.cat([half, half], dim=0)
+#     model_out = model(combined, ts, **kwargs)
+#     eps, rest = model_out[:, :3], model_out[:, 3:]
+#     cond_eps, uncond_eps = th.split(eps, len(eps) // 2, dim=0)
+#     half_eps = uncond_eps + guidance_scale * (cond_eps - uncond_eps)
+#     eps = th.cat([half_eps, half_eps], dim=0)
+#     return th.cat([eps, rest], dim=1)
 
-# Sample from the base model.
-model.del_cache()
-samples = diffusion.p_sample_loop(
-    model_fn,
-    (full_batch_size, 3, options["image_size"], options["image_size"]),
-    device=device,
-    clip_denoised=True,
-    progress=True,
-    model_kwargs=model_kwargs,
-    cond_fn=None,
-)[:batch_size]
-model.del_cache()
+# # Sample from the base model.
+# model.del_cache()
+# samples = diffusion.p_sample_loop(
+#     model_fn,
+#     (full_batch_size, 3, options["image_size"], options["image_size"]),
+#     device=device,
+#     clip_denoised=True,
+#     progress=True,
+#     model_kwargs=model_kwargs,
+#     cond_fn=None,
+# )[:batch_size]
+# model.del_cache()
 
-# Show the output
-show_images(samples)
+# # Show the output
+# show_images(samples)
 
-##############################
-# Upsample the 64x64 samples #
-##############################
+# ##############################
+# # Upsample the 64x64 samples #
+# ##############################
 
-tokens = model_up.tokenizer.encode(prompt)
-tokens, mask = model_up.tokenizer.padded_tokens_and_mask(
-    tokens, options_up['text_ctx']
-)
+# tokens = model_up.tokenizer.encode(prompt)
+# tokens, mask = model_up.tokenizer.padded_tokens_and_mask(
+#     tokens, options_up['text_ctx']
+# )
 
-# Create the model conditioning dict.
-model_kwargs = dict(
-    # Low-res image to upsample.
-    low_res=((samples+1)*127.5).round()/127.5 - 1,
+# # Create the model conditioning dict.
+# model_kwargs = dict(
+#     # Low-res image to upsample.
+#     low_res=((samples+1)*127.5).round()/127.5 - 1,
 
-    # Text tokens
-    tokens=th.tensor(
-        [tokens] * batch_size, device=device
-    ),
-    mask=th.tensor(
-        [mask] * batch_size,
-        dtype=th.bool,
-        device=device,
-    ),
-)
+#     # Text tokens
+#     tokens=th.tensor(
+#         [tokens] * batch_size, device=device
+#     ),
+#     mask=th.tensor(
+#         [mask] * batch_size,
+#         dtype=th.bool,
+#         device=device,
+#     ),
+# )
 
-# Sample from the base model.
-model_up.del_cache()
-up_shape = (batch_size, 3, options_up["image_size"], options_up["image_size"])
-up_samples = diffusion_up.ddim_sample_loop(
-    model_up,
-    up_shape,
-    noise=th.randn(up_shape, device=device) * upsample_temp,
-    device=device,
-    clip_denoised=True,
-    progress=True,
-    model_kwargs=model_kwargs,
-    cond_fn=None,
-)[:batch_size]
-model_up.del_cache()
+# # Sample from the base model.
+# model_up.del_cache()
+# up_shape = (batch_size, 3, options_up["image_size"], options_up["image_size"])
+# up_samples = diffusion_up.ddim_sample_loop(
+#     model_up,
+#     up_shape,
+#     noise=th.randn(up_shape, device=device) * upsample_temp,
+#     device=device,
+#     clip_denoised=True,
+#     progress=True,
+#     model_kwargs=model_kwargs,
+#     cond_fn=None,
+# )[:batch_size]
+# model_up.del_cache()
 
-# Show the output
-show_images(up_samples)
+# # Show the output
+# show_images(up_samples)
 
 def function(text):
   print("Function Called")
